fb7e55d2246940740553aced00f8be40a9fe385e
Alexander Pumarin <alexander@pumarin.se>
2023-09-21 12:27:51 +0200

    Second push

---
+++ src/com/alexander/github/Change.java
@@ -0,0 +1,7 @@
+package com.alexander.github;
+
+public class Change {
+    public static void change(){
+        System.out.println("This is the change");
+    }
+}
--- src/com/alexander/github/Main.java
+++ src/com/alexander/github/Main.java
@@ -3,5 +3,7 @@ package com.alexander.github;
 public class Main {
     public static void main(String[] args) {
         System.out.println("This is a test");
+
+        Change.change();
     }
 }
